79122ae6733fc63557a033001a2f4202b20cc2be
Mxolisi <ngwenyaharris34@gmail.com>
2023-04-18 22:57:20 +0200

    Cleaned up the Departments component

--- web-app/src/Components/Sections/Department/index.js
+++ web-app/src/Components/Sections/Department/index.js
@@ -1,22 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Button, ButtonGroup, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { useDispatch, useSelector } from 'react-redux';
-import { toggleDeptForm, setOpenedItemId } from '../../../redux/appState/appSlice';
-import AddDepartment from '../Forms/AddDepartment';
-import { Alert } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
-import { Blocks } from 'react-loader-spinner';
 import './styles.css';
 import MaterialTable from "@material-table/core";
 import { ExportCsv, ExportPdf } from "@material-table/exporters";
+import { useSelector } from 'react-redux';
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -31,17 +18,9 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Department = (props) => {
-  const classes = useStyles();
-  const theme = useTheme();
-  const dispatch = useDispatch();
-  const [addItem, setAddItem] = useState(false);
-  const [openDialog, setOpenDialog] = useState(false);
   const addDepartmentForm = useSelector((state) => state.app.appState);
-  const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
   const appData = addDepartmentForm.appData;
-
   const [tableData, setTableData] = useState([])
-
   const columns = [
     { title: "Department Name", field: "departmentName" },
     { title: "Description", field: "description" },
@@ -60,6 +39,7 @@ const Department = (props) => {
       <MaterialTable style={{width : '1200'}}
         title="Department List"
         data={tableData}
+        editable={true}
         columns={columns} options={{
           rowStyle :(data,index)=> index % 2 === 0 ? {backgroundColor: "#f5f5f5"}: null,
           headerStyle: {
